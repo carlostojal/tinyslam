bcaac4dc12a9b8df5063ab80f2bb11e28c5be247
Carlos Tojal <carlos.c.tojal@gmail.com>
2025-02-16 19:25:21 +0000

    add the Point2D class

---
+++ src/geometry/Point2D.cpp
@@ -0,0 +1,60 @@
+
+/*
+MIT License
+
+Copyright (c) 2025 Carlos Cabaço Tojal
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include <tinyslam/geometry/Point2D.hpp>
+
+using namespace tinyslam::geoemtry;
+
+Point2D::Point2D(float x, float y, float theta) {
+	this->x = x;
+	this->y = y;
+	this->theta = theta;
+}
+
+float Point2D::getX() const {
+	return this->x;
+}
+
+float Point2D::getY() const {
+	return this->y;
+}
+
+float Point2D::getTheta() const {
+	return this->theta;
+}
+
+void Point2D::setX(float x) {
+	this->x = x;
+}
+
+void Point2D::setY(float y) {
+	this->y = y;
+}
+
+void Point2D::setTheta(float theta) {
+	this->theta = theta;
+}
+
+
