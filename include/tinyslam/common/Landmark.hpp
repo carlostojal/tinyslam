eedb0cca4e31d431691cc40307065a2435d04588
Carlos Tojal <carlos.c.tojal@gmail.com>
2025-02-16 19:48:13 +0000

    create a landmark class

---
+++ include/tinyslam/common/Landmark.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+/*
+MIT License
+
+Copyright (c) 2025 Carlos Cabaço Tojal
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+namespace tinyslam::common {
+
+	/**
+	 * @class Landmark
+	 *
+	 * @brief Represents a map landmark with a unique identifier.
+	 */
+	class Landmark {
+
+		protected:
+			/// Landmark identifier.
+			unsigned int id;
+
+		public:
+
+			/**
+			 * @brief Landmark constructor.
+			 * 
+			 * @param id Landmark identifier.
+			 */
+			Landmark(unsigned int id);
+
+			/**
+			 * @brief "id" getter.
+			 *
+			 * @return Landmark identifier.
+			 */
+			unsigned int getID() const;
+
+			/**
+			 * @brief "id" setter.
+			 *
+			 * @param id Identifier to set.
+			 */
+			void setID(unsigned int id);
+	};
+}
